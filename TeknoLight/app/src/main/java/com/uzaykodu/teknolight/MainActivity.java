f39111ff620fa78bdeeaf4bd7e9255fdd161fa92
mahmutenestanyeri <mahmutenestanyeri@hotmail.com>
2021-05-31 23:57:37 +0300

    .
    .

---
+++ TeknoLight/app/src/main/java/com/uzaykodu/teknolight/MainActivity.java
@@ -0,0 +1,78 @@
+package com.uzaykodu.teknolight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView PbImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner dropdown = findViewById(R.id.spinner1);
+        Button btnImageUpload = findViewById(R.id.btn_Yukle);
+        PbImage= findViewById(R.id.PbImage);
+//create a list of items for the spinner.
+        String[] items = new String[]{"JPEG", "JPEG 2000", "JPEG XL","PNG"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        btnImageUpload.setOnClickListener(btnOnClickListener);
+
+
+    }
+    public View.OnClickListener btnOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            final int ACTIVITY_SELECT_IMAGE = 1234;
+            startActivityForResult(i, ACTIVITY_SELECT_IMAGE);
+        }
+    };
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode) {
+            case 1234:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = data.getData();
+                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                    Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    String filePath = cursor.getString(columnIndex);
+                    cursor.close();
+
+                    Bitmap yourSelectedImage = BitmapFactory.decodeFile(filePath);
+                       PbImage.setImageBitmap(yourSelectedImage);
+                }
+        }
+
+    };
+}
